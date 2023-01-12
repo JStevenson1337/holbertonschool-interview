4dbffb7f637b3a8628ec459a573dc7c9524ac2f9
Jeremy Stevenson <jstevenson918@gmail.com>
2023-01-12 05:55:58 -0600

    Started module

---
+++ 0-lockboxes.py/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+LockBoxes
+Can all the boxes be Opened?
+"""
+import itertools
+ 
+
+def canUnlockAll(boxes):
+	boxes = [[]]
+	unlocked = True
+	locked = False
+
+
+
+if __name__ == "__main__":
+
+canUnlockAll(boxes)
