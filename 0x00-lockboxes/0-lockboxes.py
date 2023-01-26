b5efd99171a4d1904a993da393cf1fa77e0ae202
jstevenson1337 <jmstevenson918@gmail.com>
2023-01-25 18:46:48 -0600

    commit before making adjustment

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from arepl_dump import dump
-"""
-
-"""
-boxes = [ [10, 3, 8, 9, 6, 5, 8, 1], [8, 5, 3, 7, 1, 8, 6], [5, 1, 9, 1], [], [6, 6, 9, 4, 3, 2, 3, 8, 5], [9, 4], [4, 2, 5, 1, 1, 6, 4, 5, 6], [9, 5, 8, 8], [6, 2, 8, 6] ]
-
-def canUnlockAll():
-    # TODO: Choose algorythm
-
-    print(boxes)
-    i = 1
-    counter = 0
-    isUnlocked = []
-    result = 0
-    
-    for innerList in boxes:
-        print(i, innerList)
-        i += 1
-        counter += 1
-        while innerList:
-            for j in innerList:    
-                if i == j:
-                    result = 1
-                else:
-                    result = -1
-            isUnlocked.append(result)
-        print(result)
 
 
+def canUnlockAll(boxes):
+    '''
+    Method that determines if all the boxes can be opened.
+    '''
+    if (len(boxes) == 0):
+        return False
+    box = list(range(len(boxes)))
+    key = [False]*len(boxes)
+    keys = [0]
+    key[0] = True
 
+    for i in keys:
+        for j in boxes[i]:
+            if j in box:
+                if j not in keys:
+                    keys.append(j)
+                    key[j] = True
 
-
+    if False in key:
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    canUnlockAll()
+    canUnlockAll(boxes)
