b73f45bf01fb6ccd019f087636b2b0dd4fdd89fa
jstevenson1337 <jmstevenson918@gmail.com>
2023-01-29 15:59:09 -0600

    Push all from Interview sub Repos

--- 0x02-minoperations/0-minoperations.py
+++ 0x02-minoperations/0-minoperations.py
@@ -4,7 +4,10 @@
 TODO: Doc String 
 """
 def minOperations(n):
+    # TODO: validate the use of a buffer
     buff = 1024 # running buffer
 
 
     # TODO: Return num of operations or 0 if fail
+
+# TODO: add if main statment for the module
---
+++ 0x03-sandpiles/0-sandpiles.c
@@ -0,0 +1,11 @@
+// TODO: Add: main header
+
+// TODO: Document Function
+
+// TODO: Define funtion
+
+// TODO: Fefine Return
+
+// TODO: Create Makefile
+
+// TODO: Add: main.c Driver 
