1a8a0817f1dd55a60d4a8a44bc762ff4df89bb75
Jeremy Stevenson <jstevenson918@gmail.com>
2023-02-08 22:02:24 -0600

    Checking Solution

--- 0x02-minoperations/0-minoperations.py
+++ 0x02-minoperations/0-minoperations.py
@@ -1,13 +1,44 @@
 #!/usr/bin/python3
 # *~ UTF8 ~* #
+
 """
-TODO: Doc String 
+     there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+
 """
+import sys
 def minOperations(n):
-    # TODO: validate the use of a buffer
-    buff = 1024 # running buffer
+    count= ['H']
+    current = ['H']
+    iteration = 0
 
+    if n <= 1 or isinstance(n, int) is False:
+        return 0
+    elif n > sys.maxsize:
+        count = 1
+        current = 1
+        while count < n:
+            if (n % (count * 2) == 0) or (n % count == 0):
+                current = count
+                count * 2
+                iteration += 2
+            else:
+                count += current
+                iteration += 1
+    else:
+        while len(count) < n:
+            double = (2 * len(count))
+            if (n % double == 0):
+                current = list(count)
+                count.extend(count)
+                iteration += 2
+            elif (n % len(count) == 0):
+                current = list(count)
+                count.extend(count)
+                iteration += 2
+            else:
+                count.extend(current)
+                iteration += 1
+    return iteration
 
-    # TODO: Return num of operations or 0 if fail
-
-# TODO: add if main statment for the module
+if"__name__"=="__main__":
+    minOperations()
--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -3,8 +3,19 @@
 
 //TODO: add libraries
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 /* Prototype */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sum_grids(int grid1[3][3], int grid2[3][3]);
+int check_grids(int grid1[3][3]);
+void print_grid(int grid[3][3]);
+void change_grids(int grid1[3][3]);
+
+
+
 
 
 #endif /*__SANDPILES_H__*/
