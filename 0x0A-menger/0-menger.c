6f640b35bb3e25e29edca3be97f3af04777bf266
jstevenson1337 <jmstevenson918@gmail.com>
2023-04-24 21:44:35 -0500

    testing menger

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -6,16 +6,23 @@
  */
 void menger(int level)
 {
-        const char octothorpe = '#';
-	
-	if (level < 0)
-	{
-		printf("%c\n", octothorpe);
-	}
+    if (level < 0)
+        return;
 
-	while (level > 0)
-	{
-		printf("%c", octothorpe);;;
-	}
+    int size = (int)pow(3, level);
 
+    // Draw the sponge
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            // Check if cell (i,I j) is not an empty cell 
+            if (((i / (int)pow(3, level - 1)) % 3 == 1) &&
+                ((j / (int)pow(3, level - 1)) % 3 == 1))
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("\n");
+    }
 }
