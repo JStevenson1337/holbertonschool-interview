6551f2f49bb589850ee2552610e11cef26c59066
jstevenson1337 <jmstevenson918@gmail.com>
2023-04-24 21:49:24 -0500

    Changed C style comments

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -11,12 +11,12 @@ void menger(int level)
 
     int size = (int)pow(3, level);
 
-    // Draw the sponge
+    /* Draw the sponge */
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            // Check if cell (i,I j) is not an empty cell 
+            /* Check if cell (i,I j) is not an empty cell */
             if (((i / (int)pow(3, level - 1)) % 3 == 1) &&
                 ((j / (int)pow(3, level - 1)) % 3 == 1))
                 printf(" ");
